92017c8c2ffa75821d669febb30937d65a1bc217
SirArnoldB <legendaryarnoldb@gmail.com>
2022-12-14 00:36:04 -0500

    update urls and templates

--- meeting_planner/meeting_planner/urls.py
+++ meeting_planner/meeting_planner/urls.py
@@ -14,15 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from website.views import welcome, date, about
-from meetings.views import detail
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", welcome),
+    path("", welcome, name="welcome"),
     path("date", date),
     path("about", about),
-    path("meetings/<int:id>", detail, name="detail"),
+    path('meetings/', include('meetings.urls')),
 ]
--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Meeting
+from .models import Meeting, Room
 
 def detail(request, id):
     meeting = get_object_or_404(Meeting, pk=id)
     return render(request, "meetings/detail.html", {"meeting": meeting})
+
+def rooms(request):
+    return render(request, "meetings/rooms.html", {"rooms": Room.objects.all()})
